7263a4a1fb933c2f61a5d3350b680fee3261935b
mohamed-hossam13 <muhamad.hossam1346@gmail.com>
2024-09-20 00:18:43 +0300

    Solving Second problem in the first sheet

---
+++ Week 1/B-Square String/Main.java
@@ -0,0 +1,140 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    final static PrintWriter pw = new PrintWriter(System.out);
+    final static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+        int t = sc.nextInt(); //number of test cases
+        String[] testCases = new String[t];
+        boolean[] answer = new boolean[t];
+
+        //Reading the test cases
+        for (int i = 0; i < t; ++i) {
+            testCases[i] = sc.next();
+            answer[i] = true; //Assuming that all strings are squares
+        }
+
+        for (int i = 0; i < t; ++i) {
+            int size = testCases[i].length();
+            int lastIndex = size - 1;
+            int midIndex = lastIndex / 2;
+
+            if (size % 2 != 0) //The case if the size of the string is odd
+                answer[i] = false;
+
+            else {
+                for (int j = 0; j < size / 2; ++j) {
+                    if (testCases[i].charAt(lastIndex - j) != testCases[i].charAt(midIndex - j)) {
+                        answer[i] = false;
+                        break;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < t; ++i) {
+            if (answer[i])
+                pw.println("YES");
+            else
+                pw.println("NO");
+        }
+
+
+
+
+        pw.close();
+    }
+
+    static class Scanner {
+
+        StringTokenizer st;
+        BufferedReader br;
+        public Scanner(InputStream s) {
+            br = new BufferedReader(new InputStreamReader(s));
+        }
+        public Scanner(String file) throws IOException {
+            br = new BufferedReader(new FileReader(file));
+        }
+        public Scanner(FileReader r) {
+            br = new BufferedReader(r);
+        }
+        public String next() throws IOException {
+            while (st == null || !st.hasMoreTokens())
+                st = new StringTokenizer(br.readLine());
+            return st.nextToken();
+        }
+        public String readAllLines(BufferedReader reader) throws IOException {
+            StringBuilder content = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line);
+                content.append(System.lineSeparator());
+            }
+            return content.toString();
+        }
+        public int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+        public long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+        public String nextLine() throws IOException {
+            return br.readLine();
+        }
+        public double nextDouble() throws IOException {
+            String x = next();
+            StringBuilder sb = new StringBuilder("0");
+            double res = 0, f = 1;
+            boolean dec = false, neg = false;
+            int start = 0;
+            if (x.charAt(0) == '-') {
+                neg = true;
+                start++;
+            }
+            for (int i = start; i < x.length(); i++)
+                if (x.charAt(i) == '.') {
+                    res = Long.parseLong(sb.toString());
+                    sb = new StringBuilder("0");
+                    dec = true;
+                } else {
+                    sb.append(x.charAt(i));
+                    if (dec)
+                        f *= 10;
+                }
+            res += Long.parseLong(sb.toString()) / f;
+            return res * (neg ? -1 : 1);
+        }
+        public long[] nextlongArray(int n) throws IOException {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextLong();
+            return a;
+        }
+        public Long[] nextLongArray(int n) throws IOException {
+            Long[] a = new Long[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextLong();
+            return a;
+        }
+        public int[] nextIntArray(int n) throws IOException {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextInt();
+            return a;
+        }
+        public Integer[] nextIntegerArray(int n) throws IOException {
+            Integer[] a = new Integer[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextInt();
+            return a;
+        }
+        public boolean ready() throws IOException {
+            return br.ready();
+        }
+
+    }
+}
